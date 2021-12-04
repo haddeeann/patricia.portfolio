02fdce6fb4ac4188a5535cb3c930ba8230a60833
Haddee Ann <haddeeann@gmail.com>
2021-12-04 06:44:32 -0500

    added in some animation for the header

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,10 +1,63 @@
-import React from 'react';
+import React, { useRef, useLayoutEffect } from 'react';
+import { gsap } from 'gsap';
 
 const Introduction = () => {
+    const el = useRef();
+    const hello = 'hello there...'.split(' ');
+    const scroll = 'please scroll down'.split(' ');
+    const helloText = hello.map((l, index) => {
+        return <span key={index} className='hello'>{l}&nbsp;</span>;
+    });
+
+    const scrollText = scroll.map((w, index) => {
+        return <span key={index} className='scrollText'>{w}&nbsp;</span>;
+    });
+
+    useLayoutEffect(() => {
+        const tl = gsap.timeline();
+        // Target any descendant with the class of .box - no matter how far down the descendant tree. Uses el.current.querySelectorAll() internally
+        tl.fromTo((".intro"), {
+          opacity: '0',
+          duration: 7,
+          css: {
+              'filter': 'grayscale(100%)'
+          }
+        },
+        {
+            opacity: '0.90',
+            duration: 7,
+            css: {
+                'filter': 'grayscale(20%)'
+            }
+        }, 0.00);
+
+        tl.from('.hello', {
+            opacity: '0',
+            duration: 3,
+            stagger: 0.4
+        }, 0.5);
+
+        tl.from('.scrollText', {
+            opacity: '0',
+            duration: 3,
+            stagger: 0.4
+        }, 2.50);
+    });
+
     return (
-        <div className='box intro'> 
-            <div className='inner-box' id='intro'>
-                <div className='content'></div>
+        <div>
+            <div className='box intro' ref={el}> 
+                <div className='inner-box' id='intro'>
+                    <div className='content'></div>
+                </div>
+                <div className='imageTextBox'>
+                    <div>
+                        {helloText}
+                    </div>
+                    <div>
+                        {scrollText}
+                    </div>
+                </div>
             </div>
         </div>
     );
