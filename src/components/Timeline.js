13c2728beb53c0e6f98d1d7f2b74e7f475be2b90
HaddeeAndd <haddeeann@gmail.com>
2023-12-10 16:06:14 -0500

    adding better descriptions to the jobs, more accurate timelines and center text on mobile

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -5,14 +5,15 @@ const Timeline = () => {
         <div className='box'>
             <div className='inner-box' id='time'>
                 <div className='content'>
-                    <h3>A TIMELINE</h3>
-                    <h4>What have I been up to?</h4>
+                    <h2>PROFESSIONAL TIMELINE</h2>
                     <div className='time-box'>
                         <div>
                             <div className='edit-icon'><i className="fa fa-pencil" aria-hidden="true"></i></div>
                         </div>
                         <div className='time-box-inner'>
-                            <h4>Fullstack Developer at PivotCX</h4>  <span>Februrary 2022 to Present</span>
+                            <h3>Fullstack Developer</h3>
+                            <h4>PivotCX</h4>
+                            <div>Februrary 2022 to Present</div>
                             <p>We help recruiters reach the right candidates at fast speeds.</p>
                         </div>
                     </div>
@@ -21,7 +22,9 @@ const Timeline = () => {
                             <div className='edit-icon'><i className="fa fa-pencil" aria-hidden="true"></i></div>
                         </div>
                         <div className='time-box-inner'>
-                            <h4>Software Developer at Yellow Umbrella Creators</h4>  <span>January 2021 to Present</span>
+                            <h3>Software Developer</h3>
+                            <h4>Yellow Umbrella Creators</h4>
+                            <div>January 2021 to Present</div>
                             <p>Yellow Umbrella Creators is my personal project company.</p>
                         </div>
                     </div>
@@ -30,7 +33,9 @@ const Timeline = () => {
                             <div className='edit-icon'><i className="fa fa-pencil" aria-hidden="true"></i></div>
                         </div>
                         <div className='time-box-inner'>
-                            <h4>Software Developer at Constant Contact</h4>  <span>March 2020 to December 2020</span>
+                            <h3>Software Developer</h3>
+                            <h4>SharpSpring acquired by Constant Contact</h4>
+                            <div>March 2020 to January 2021</div>
                             <p>Constant Contact helps achieve automation in communicating with customers and marketing.</p>
                         </div>
                     </div>
@@ -39,7 +44,9 @@ const Timeline = () => {
                             <div className='edit-icon'><i className="fa fa-pencil" aria-hidden="true"></i></div>
                         </div>
                         <div className='time-box-inner'>
-                            <h4>Software Developer at Teledyne FLIR</h4>  <span>March 2020 to December 2020</span>
+                            <h3>Software Developer</h3>
+                            <h4>Altavian acquired by Teledyne FLIR</h4>
+                            <div>September 2017 to February 2020</div>
                             <p>
                                 Teledyne FLIR is a global leader in the design, manufacture, and marketing of thermal imaging infrared cameras for various applications, such as surveillance, security, industrial, and scientific.
                             </p>
