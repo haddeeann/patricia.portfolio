856f199f1561d55ec385d2796c3d1ed2ff1e7b19
Patricia Green <patricia@pivotcx.io>
2025-06-22 15:31:49 -0400

    revise to a more modern vite system

--- src/sections/Portfolio.jsx
+++ src/sections/Portfolio.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import data_frog from '../images/data_frog_web_page.png';
 import adventure_dog from '../images/adventure_dog_web_page.png';
-import distraction_free from '../images/distractionfree_web_page.png'
+import distraction_free from '../images/distractionfree_web_page.png';
 import flex_box from '../images/flex_box_web_page.png';
+import take_note from '../images/take_note.png';
 
 const Portfolio = () => {
     return (
@@ -11,10 +12,22 @@ const Portfolio = () => {
                 <div className='content'>
                     <h2>Portfolio</h2>
                     <div className='portfolio-section'>
+                        <a href='https://illustrious-biscochitos-cdd8ac.netlify.app/auth' target='_blank' rel='noreferrer'>
+                            <div className='portfolio-row'>
+                                <div>
+                                    <img width='100%' height='100%' src={take_note} alt='Note Taking App'/>
+                                </div>
+                                <div className='description'>
+                                    A basic note taking app, with login and register with Firebase db.
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div className='portfolio-section'>
                         <a href='https://foxycss.netlify.app/' target='_blank' rel='noreferrer'>
                             <div className='portfolio-row'>
                                 <div>
-                                    <img width='100%' height='100%' src={flex_box} alt='Flex Box CSS' />
+                                    <img width='100%' height='100%' src={flex_box} alt='Flex Box CSS'/>
                                 </div>
                                 <div className='description'>
                                     A CSS flexbox page with all the basic flexbox displayed.
@@ -26,7 +39,8 @@ const Portfolio = () => {
                         <a href='https://distractionfree.netlify.app/' target='_blank' rel='noreferrer'>
                             <div className='portfolio-row'>
                                 <div>
-                                    <img width='100%' height='100%' src={distraction_free} alt='Distraction Free Learning' />
+                                    <img width='100%' height='100%' src={distraction_free}
+                                         alt='Distraction Free Learning'/>
                                 </div>
                                 <div className='description'>
                                     A website for my own use for distraction free learning about data science.
@@ -39,10 +53,11 @@ const Portfolio = () => {
                         <a href='https://data-frog.netlify.app/numpy/1_create_array/' target='_blank' rel='noreferrer'>
                             <div className='portfolio-row'>
                                 <div>
-                                    <img width='100%' height='100%' src={data_frog} alt='Data Frog' />
+                                    <img width='100%' height='100%' src={data_frog} alt='Data Frog'/>
                                 </div>
                                 <div className='description'>
-                                    A website for writing about learning about data science. I write the info based on my learning.
+                                    A website for writing about learning about data science. I write the info based on
+                                    my learning.
                                 </div>
                             </div>
                         </a>
@@ -51,11 +66,13 @@ const Portfolio = () => {
                         <a href='https://adventure-dog.netlify.app/' target='_blank' rel='noreferrer'>
                             <div className='portfolio-row'>
                                 <div>
-                                    <img width='100%' height='100%'  src={adventure_dog} alt='Adventure Dog' />
+                                    <img width='100%' height='100%' src={adventure_dog} alt='Adventure Dog'/>
                                 </div>
                                 <div className='description'>
-                                    A website for articles about taking care of dogs. I'm an avid dog fan, so this is just for fun.
-                                    I used a small blogging technology that allows blog articles to be written in mark down.
+                                    A website for articles about taking care of dogs. I'm an avid dog fan, so this is
+                                    just for fun.
+                                    I used a small blogging technology that allows blog articles to be written in mark
+                                    down.
                                 </div>
                             </div>
                         </a>
